5f5c76bd8ecf7db9f5a49f19320726d612cb81d1
Mary Olowu <maryolowu@marys-mbp-2.mynetworksettings.com>
2024-06-06 13:04:09 -0500

    ADDED Medusa spec

---
+++ commands.sh
@@ -0,0 +1,8 @@
+│ password: LeL0372Pe3oo9PAQoh3X                                                                                │
+│ username: demo-superuser   
+
+10.1.1.216
+
+demo-dc1-default-sts-0
+
+cqlsh -u demo-superuser -p LeL0372Pe3oo9PAQoh3X 
